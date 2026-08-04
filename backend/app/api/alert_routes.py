c6ad25344e0759e2027ce746770f89f173c86643
Shashank M <133743836+shashankm31@users.noreply.github.com>
2026-08-04 09:55:04 +0000

    Updated

--- backend/app/api/alert_routes.py
+++ backend/app/api/alert_routes.py
@@ -1,30 +1,34 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.schemas.alert_schema import AlertCreate
+from app.schemas.alert_schema import AlertCreate, AlertResponse
 from app.database.database import get_db
 from app.models.alert_model import Alert
 
 
-
 router = APIRouter()
 
-
-    
-    
-@router.post("/alerts")
-def create_alert(alert: AlertCreate,
-                 db: Session = Depends(get_db)
-):
-    
+@router.post("/alerts", response_model = AlertResponse)
+def create_alert(
+    alert: AlertCreate,
+    db: Session = Depends(get_db)
+):  
     db_alert = Alert(
         device = alert.device,
         status = alert.status
     )
-    
     db.add(db_alert)
     db.commit()
     db.refresh(db_alert)
     
     return db_alert
+
+@router.get("/alerts", response_model = list[AlertResponse])
+def get_alerts(
+    db: Session = Depends(get_db)
+):
+    alerts = db.query(Alert).all()
+    return alerts
+
+
     
--- backend/app/schemas/alert_schema.py
+++ backend/app/schemas/alert_schema.py
@@ -1,5 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class AlertCreate(BaseModel):
     device: str
     status: str
+    
+class AlertResponse(BaseModel):
+    id: int
+    device: str
+    status: str
+    
+model_config = ConfigDict(from_attributes = True)
