9db249408cdf33f3bc458bbff057cfbf38820d96
Shashank M <133743836+shashankm31@users.noreply.github.com>
2026-09-23 00:01:08 +0000

    cloudwatch updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.alert_routes import router
 from app.api.auth_routes import router as auth_router
+from app.api.cloudwatch_routes import router as cloudwatch_router
 
 from app.database.database import Base, engine
 from app.models.alert_model import Alert
@@ -16,6 +17,8 @@ Base.metadata.create_all(bind = engine)
 app.include_router(router)
 
 app.include_router(auth_router)
+
+app.include_router(cloudwatch_router)
 
 
 #Home API
